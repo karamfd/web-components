6bbecf37d0cd4c16f86fdd414055c27899aa8909
Karam Fahad <5566383+karamfd@users.noreply.github.com>
2023-11-11 14:45:29 -0500

    Simplified components: No shadow DOM

--- components/Footer.js
+++ components/Footer.js
@@ -1,29 +1,14 @@
-const template = document.createElement("template");
-const style = document.createElement("style");
-
-template.innerHTML = /* html */ `
-  <footer>
-    <small>Your footer text goes here</small>
-  </footer>
-`;
-
-style.textContent = /* css */ `
-  /* footer-component local style */
-  :host {
-    margin-block-start: auto;
-    background-color: pink;
-    padding: 1rem;
-    text-align: center;
-  }
-`;
-
 class Footer extends HTMLElement {
   constructor() {
     super();
+  }
 
-    const shadow = this.attachShadow({ mode: "closed" });
-    shadow.append(template.content.cloneNode(true));
-    shadow.append(style);
+  connectedCallback() {
+    this.innerHTML = `
+      <footer>
+        <small>Copyright &copy; 2023. All Rights Reserved.</small>
+      </footer>
+    `;
   }
 }
 
--- components/Nav.js
+++ components/Nav.js
@@ -1,40 +1,19 @@
-const template = document.createElement("template");
-const style = document.createElement("style");
-
-template.innerHTML = /* html */ `
-  <nav>
-    <ul>
-      <li><a href="/">home</a></li>
-      <li><a href="/about">about</a></li>
-      <li><a href="/contact">contact</a></li>
-    </ul>
-  </nav>
-`;
-
-style.textContent = /* css */ `
-  nav li {
-    margin-block: 1rem;
-  }
-
-  nav a {
-    background-color: yellow;
-    color: blue;
-  }
-
-  @media screen and (max-width: 700px) {
-    nav a {
-      color: green;
-    }
-  }
-`;
-
 class Nav extends HTMLElement {
   constructor() {
     super();
+  }
 
-    const shadow = this.attachShadow({ mode: "open" });
-    shadow.append(template.content.cloneNode(true));
-    shadow.append(style);
+  // When the component is loaded into the DOM
+  connectedCallback() {
+    this.innerHTML = `
+      <nav>
+        <ul>
+          <li><a href="/">home</a></li>
+          <li><a href="/about">about</a></li>
+          <li><a href="/contact">contact</a></li>
+        </ul>
+      </nav>
+    `;
   }
 }
 

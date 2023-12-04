bd0bc35123a794a01497338a68da8a21e67f8691
Karam Fahad <5566383+karamfd@users.noreply.github.com>
2023-12-03 20:41:22 -0500

    Simplified web component

--- components/Title.js
+++ components/Title.js
@@ -3,16 +3,11 @@ class Title extends HTMLElement {
     super();
   }
   connectedCallback() {
-    const template = document.createElement("template");
-
-    template.innerHTML = `
+    this.attachShadow({ mode: "closed" }).innerHTML = `
       <h1>
         <slot name="title">Title Page</slot>
       </h1>
     `;
-
-    const shadow = this.attachShadow({ mode: "closed" });
-    shadow.appendChild(template.content.cloneNode(true));
   }
 }
 

90fbad620a07409ba6255ebf160bd0c2919fe5bb
Karam Fahad <5566383+karamfd@users.noreply.github.com>
2024-01-10 17:13:37 -0400

    CSS applies to Title component only

--- components/Title.js
+++ components/Title.js
@@ -1,7 +1,15 @@
 class Title extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: "closed" }).innerHTML = `
+
+    this.root = this.attachShadow({ mode: "closed" });
+
+    const styleSheet = new CSSStyleSheet();
+    styleSheet.replaceSync("h1 { color: red; }");
+
+    this.root.adoptedStyleSheets = [styleSheet];
+
+    this.root.innerHTML = `
       <h1>
         <slot name="title">Title Page</slot>
       </h1>
